06f3cfd906747b3fdacb30ef9b9b88e025c5f4e6
plger <pl.germain@gmail.com>
2021-05-07 13:03:28 +0200

    changed 'cores' argument, added missing imports

--- R/runFullScan.R
+++ R/runFullScan.R
@@ -2,49 +2,72 @@
 #'
 #' Runs a full miRNA scan on all protein-coding transcripts (or UTRs) of an
 #' annotation.
-#' 
+#'
 #' @param annotation A \code{\link{ScanMiRAnno}} object
 #' @param mods An optional `KdModelList` (defaults to the one in `annotation`)
-#' @param annoFilter An optional `AnnotationFilter` or `AnnotationFilterList` to 
+#' @param annoFilter An optional `AnnotationFilter` or `AnnotationFilterList` to
 #' filter the set of transcripts to be extracted
-#' @param extract Which parts of the transcripts to extract. For `UTRonly` 
-#' (default) only the 3' UTR regions are extracted, `withORF` additionally 
+#' @param extract Which parts of the transcripts to extract. For `UTRonly`
+#' (default) only the 3' UTR regions are extracted, `withORF` additionally
 #' extracts the coding regions, and `exons` extracts all exons
 #' @param shadow The size of the ribosomal shadow at the UTR starts
-#' @param cores The number of threads to use
+#' @param cores The number of threads to use. Alternatively accepts a
+#' \code{\link[BiocParallel]{BiocParallelParam-class}}, as for instance produced
+#' by \code{\link[BiocParallel]{MulticoreParam}}.
 #' @param maxLogKd The maximum log_kd of sites to report
 #' @param save.path Optional, the path to which to save the results
 #' @param ... Arguments passed to `scanMiR::findSeedMatches`
 #'
 #' @export
-#' @importFrom BiocParallel SerialParam MulticoreParam
-#' @importFrom GenomicFeatures extractTranscriptSeqs threeUTRsByTranscript cdsBy
 #' @import Biostrings scanMiR
+#' @importFrom BiocParallel SerialParam MulticoreParam bpnworkers
+#' @importFrom GenomicFeatures extractTranscriptSeqs threeUTRsByTranscript
+#' @importFrom GenomicFeatures exonsBy cdsBy
+#' @importFrom GenomicRanges strand
 #' @importFrom S4Vectors metadata metadata<-
-#' @examples 
+#' @importFrom stats setNames
+#' @importFrom AnnotationFilter AnnotationFilterList SeqNameFilter
+#' @examples
 #' # not run
 #' # anno <- ScanMiRAnno("Rnor_6")
 #' # seq <- runFullScan( annotation=anno )
-runFullScan <- function(annotation, mods=NULL, annoFilter = NULL, 
+runFullScan <- function(annotation, mods=NULL, annoFilter = NULL,
                         extract=c("UTRonly", "withORF", "exons"),
                         onlyCanonical=TRUE, shadow=15, cores=1,
                         maxLogKd=c(-1,-1.5), save.path=NULL, ...){
-  message("Loading annotation")
+
   stopifnot(is(annotation, "ScanMiRAnno"))
   if(is.null(mods)) mods <- annotation$models
   stopifnot(is(mods,"KdModelList"))
+
+  if(is.numeric(cores) && length(cores)==1){
+    cores <- as.integer(cores)
+    if(cores>1){
+      BP <- MulticoreParam(cores, progress=TRUE)
+    }else{
+      BP <- SerialParam()
+    }
+  }elseif(is(cores,"BiocParallelParam")){
+    BP <- cores
+  }else{
+    warning("`cores` argument of unknown type -- will be ignored.")
+    BP <- SerialParam()
+  }
+
+  message("Loading annotation")
   genome <- annotation$genome
   ensdb <- annotation$ensdb
   seqlevelsStyle(genome) <- "Ensembl"
-  
+
   # restrict to canonical chromosomes
   canonical_chroms <- seqlevels(genome)[!grepl('_', seqlevels(genome))]
   filt <- SeqNameFilter(canonical_chroms)
-  
+
   if(!is.null(annoFilter)) {
-    if(is(annoFilter, "AnnotationFilterList") || is(annoFilter, "AnnotationFilter"))
+    if(is(annoFilter, "AnnotationFilterList") ||
+       is(annoFilter, "AnnotationFilter"))
       filt <- AnnotationFilterList(filt, annoFilter)
-    else 
+    else
       stop("filter must be either `AnnotationFilter` or `AnnotationFilterList`")
   }
   extract <- match.arg(extract)
@@ -73,18 +96,14 @@ runFullScan <- function(annotation, mods=NULL, annoFilter = NULL,
       strand <- unlist(unique(strand(c(grl_ORF, grl_UTR))))
     }
   }
-  message("Scanning with ", cores, " core(s)")
-  if(cores>1){
-    BP <- MulticoreParam(cores, progress=TRUE)
-  }else{
-    BP <- SerialParam()
-  }
-  m <- findSeedMatches(seqs, mods, shadow=shadow, maxLogKd=maxLogKd, 
+
+  message("Scanning with ", bpnworkers(BP), " thread(s)")
+  m <- findSeedMatches(seqs, mods, shadow=shadow, maxLogKd=maxLogKd,
                        onlyCanonical=onlyCanonical, BP=BP, ...)
-  
+
   md <- metadata(anno$ensdb)
   md <- setNames(md$value,md$name)
-  
+
   if(is(m, "GRanges")) {
     metadata(m)$tx_info$strand <- strand[rownames(metadata(m)$tx_info)]
     metadata(m)$genome_build <- md[["genome_build"]]
