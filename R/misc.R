5a5214a36d7e951ce24e431db3b4ca34072b0b10
plger <pl.germain@gmail.com>
2021-05-31 17:49:32 +0000

    moved get8merRange, updated doc

--- R/misc.R
+++
@@ -1,9 +0,0 @@
-.get8merRange <- function(mod){
-  stopifnot(is(mod,"KdModel"))
-  mer8 <- getSeed8mers(mod$canonical.seed)
-  mer8 <- which(mer8==mod$canonical.seed)
-  fl <- rowSums(apply(scanMiR:::.flankingValues(), 2, FUN=range))
-  fl*SampleKdModel$fl[mer8]+SampleKdModel$mer8[mer8]
-}
-
-
