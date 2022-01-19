06cecde178664fb9daefa001483ba1121d646ca5
plger <pl.germain@gmail.com>
2022-01-19 08:06:14 +0000

    missing event on stypesHelp2

--- R/scanMiRserver.R
+++ R/scanMiRserver.R
@@ -581,6 +581,7 @@ scanMiRserver <- function( annotations=list(), modlists=NULL,
 
     observeEvent(input$colHelp, .getHelpModal("hitsCol"))
     observeEvent(input$stypeHelp, .getHelpModal("stypes"))
+    observeEvent(input$stypeHelp2, .getHelpModal("stypes"))
     observeEvent(input$manhattanHelp, .getHelpModal("manhattan"))
     observeEvent(input$help_collections, .getHelpModal("collections"))
     
