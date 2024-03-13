b379d6a7462c9dce03d7bb8f354910e4a7060acf
plger <pl.germain@gmail.com>
2024-03-13 20:31:46 +0100

    pre-compiled fake TxDb

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' @rdname fakeTxDb
+#' @name fakeTxDb
+#'
+#' @title Example 'fake' TxDb object
+#'
+#' @description
+#' A fake transcript database used for examples.
+#'
+#' @return a named character vector of length 1.
+NULL
